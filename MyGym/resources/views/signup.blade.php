9964a6978dd9851003b253995e31d346e715a86e
Anurag Baliyan <anuragbaliyanmzn@gmail.com>
2023-01-29 01:06:58 +0530

    Gym-laravel - signup-page-phase-1-fix-1

--- MyGym/resources/views/signup.blade.php
+++ MyGym/resources/views/signup.blade.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="css/signup.css" rel="stylesheet"> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <title>Sign Up</title>
 </head>
 <body>
@@ -39,7 +37,7 @@
 
             <div class="input-container">
                 <label class="label" for="gender">Gender:</label>
-                <select class="form-select" name="gender" id="gender" required>
+                <select name="gender" id="gender" required>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
                     <option value="Others">Others</option>
@@ -58,7 +56,7 @@
 
             <div class="input-container">
                 <label class="label" for="reason">Main Concern:</label>
-                <select style="max-width: 200px;" class="form-select" name="reason" id="reason" required>
+                <select name="reason" id="reason" required>
                     <option value="Body Building">Body Building</option>
                     <option value="Yoga">Yoga</option>
                     <option value="Weight Loss">Weight Loss</option>
